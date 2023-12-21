57b707cb7f5e0b51c36a40611fda94471c5568d8
Allen <allenshamrock37@gmail.com>
2023-12-22 01:47:57 +0300

    Completed assigment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    createToDo(e.target.new_task_description.value);
+    form.reset();
+  });
 });
+
+function createToDo(todo) {
+  let li = document.createElement("li");
+  li.textContent = todo;
+  //console.log(li)
+  document.getElementById("tasks").appendChild(li);
+}
